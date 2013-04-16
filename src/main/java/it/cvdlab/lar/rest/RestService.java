caffcd99375085ba9ec0cbef4854e78e01dca9ec
Francesco Furiani <francesco@furio.me>
2013-04-16 17:38:44 +0200

    Rename

--- src/main/java/it/cvdlab/lar/rest/RestService.java
+++ src/main/java/it/cvdlab/lar/rest/RestService.java
@@ -3,6 +3,7 @@ package it.cvdlab.lar.rest;
 import it.cvdlab.lar.model.CsrMatrix;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -14,18 +15,25 @@ import javax.ws.rs.core.UriInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Path(RestService.REST_SERVICE_URL)
 public class RestService {
     private static final Logger logger = LoggerFactory.getLogger(RestService.class);
 
-    public static final String REST_SERVICE_URL = "/service";
+    public static final String REST_SERVICE_URL = "/lar";
 
-    @Path("/create")
+    @Path("/multiply")
     @POST
     @Consumes({ MediaType.APPLICATION_FORM_URLENCODED })
     @Produces({ MediaType.APPLICATION_JSON })
-    public CsrMatrix createWorkOrder(@Context UriInfo uriInfo, MultivaluedMap<String, String> form) {
+    public CsrMatrix doMultiply(@Context UriInfo uriInfo, MultivaluedMap<String, String> form) {
 
         return new CsrMatrix();    	
     }
+    
+    @Path("/test")
+    @GET
+    @Produces({ MediaType.APPLICATION_JSON })
+    public String doTest() {
+
+        return "Che bello";    	
+    }    
 }
