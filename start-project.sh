77ba0c2407779c153581d846ff87dc49e11f0f63
Francesco Furiani <furio@cvd01.dia.uniroma3.it>
2013-05-03 16:03:03 +0200

    Fixed pom.xml Fixed startup script

--- start-project.sh
+++ start-project.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-if [[ -z "$JAVA_HOME" ]]; then
-	echo "No $JAVA_HOME set"
+if [ "$JAVA_HOME" != "" ]; then
+	mvn package
+	export MAVEN_OPTS="-Xmx8192m"
+	export LD_PRELOAD=$JAVA_HOME/jre/lib/amd64/libjsig.so
+	mvn jetty:run
+else
+	echo "No \$JAVA_HOME set"
 fi
-
-mvn package
-export MAVEN_OPTS="-Xmx8192m"
-export LD_PRELOAD=$JAVA_HOME/jre/lib/amd64/libjsig.so
-mvn jetty:run
