7c7c472b300ed8e587deea134df437e5d010889c
Francesco Furiani <francesco@furio.me>
2013-05-07 13:52:09 +0200

    Object to retain status.

---
+++ src/main/java/it/cvdlab/lar/clengine/MultiplyCLStatus.java
@@ -0,0 +1,23 @@
+package it.cvdlab.lar.clengine;
+
+public final class MultiplyCLStatus {
+	// qui il context
+	
+	// 
+	
+	// qui i ptr
+	
+	// qui i cl_mem
+	
+	// qui il counter riclabile
+	
+	// =======================
+	
+	// qui il valore di nnz
+	
+	// qui matrix a,b
+	
+	// =======================
+	
+	// il method free
+}
